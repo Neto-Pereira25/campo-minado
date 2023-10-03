185ff0b10c6aaa6f4ad5fd15b901879c057dd2fc
Jose Neto <devneto203@gmail.com>
2023-10-02 22:24:58 -0300

    Criada classe TabuleiroConsole

--- src/br/com/devNeto/cm/Aplicacao.java
+++ src/br/com/devNeto/cm/Aplicacao.java
@@ -1,18 +1,15 @@
 package br.com.devNeto.cm;
 
 import br.com.devNeto.cm.modelo.Tabuleiro;
+import br.com.devNeto.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
 		
-		tabuleiro.alterarMarcacao(4, 4);
-		tabuleiro.alterarMarcacao(4, 5);
-		tabuleiro.abrir(3, 3);
-		
-		System.out.println(tabuleiro);
 	}
 
 }
