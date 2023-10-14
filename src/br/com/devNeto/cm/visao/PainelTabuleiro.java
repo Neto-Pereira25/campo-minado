db5c46ded69fd7596deba68e91426665b72bb2a9
Jose Neto <devneto203@gmail.com>
2023-10-14 02:18:55 -0300

    Criado as classes TelaPrincipal e PainelTabuleiro

---
+++ src/br/com/devNeto/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.devNeto.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.devNeto.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
